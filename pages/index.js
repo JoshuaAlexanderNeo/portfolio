5a37249ca5bfc7980a87d9c5bf5304a1a372e597
Josh <joshuaalexanderneo@gmail.com>
2022-06-28 17:43:34 +0400

    Add contact form with formspree.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import { ResponsiveNavBar } from '../components/ResponsiveNavbar'
 import Hero from '../components/Hero'
 import Experience from '../components/Experience'
 import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+
 export default function Home() {
   return (
     <div>
@@ -22,6 +24,7 @@ export default function Home() {
       <div className='bg-gradient-to-b from-white to-blue-50'>
         <Experience />
         <Projects />
+        <Contact />
       </div>
     </div>
   )
