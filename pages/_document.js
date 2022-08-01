52131790fdcc776b8169cbd96d920ae689ed77b9
Josh <joshuaalexanderneo@gmail.com>
2022-08-01 13:06:25 +0400

    Add google fonts stylesheets

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          href='https://fonts.googleapis.com/css2?family=Playfair+Display&family=Roboto+Condensed&display=swap'
-          rel='stylesheet'></link>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
+        <link href='https://fonts.googleapis.com/css2?family=Playfair+Display&family=Roboto+Condensed&display=swap' rel='stylesheet'></link>
         <link href='https://fonts.googleapis.com/css2?family=Play&display=swap' rel='stylesheet'></link>
         <link href='https://fonts.googleapis.com/css2?family=Cardo&display=swap' rel='stylesheet'></link>
         <link href='https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz&display=swap' rel='stylesheet'></link>
