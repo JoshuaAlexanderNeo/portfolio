0bb2deec77882a4350a4aa4ed7ca7651e5b678de
Josh <joshuaalexanderneo@gmail.com>
2022-08-01 13:00:29 +0400

    Fix unescaped apostrophes

--- components/About/index.js
+++ components/About/index.js
@@ -8,12 +8,10 @@ const About = ({ quote }) => {
         <div>
           <span className='italic text-3xl'>Hello! 👋</span>
           <p className='text-justify mt-4 leading-tight tracking-tight'>
-            I'm <strong>Joshua Alexander</strong>, a full-stack developer from BC, Canada. I graduated from Acadia
-            University in 2020 with a BSc. in <strong>Computer Science</strong> (3.81 GPA). I love working with
-            Javascript frameworks, and my preferred frameworks are Next.js, <strong>React</strong>, and{' '}
-            <strong>Node.js</strong>—and recently I've been loving TailwindCSS! I'm also a big fan of the open-source
-            community, and I'm always looking to contribute to open-source projects. I also like writing small Python
-            scripts to automate small tasks, crawl websites, and other small projects.
+            I&apos;m <strong>Joshua Alexander</strong>, a full-stack developer from BC, Canada. I graduated from Acadia University in 2020 with a BSc. in <strong>Computer Science</strong> (3.81 GPA).
+            I love working with Javascript frameworks, and my preferred frameworks are Next.js, <strong>React</strong>, and <strong>Node.js</strong>—and recently I&apos;ve been loving TailwindCSS!
+            I&apos;m also a big fan of the open-source community, and I&apos;m always looking to contribute to open-source projects. I also like writing small Python scripts to automate small tasks,
+            crawl websites, and other small projects.
           </p>
 
           <div className='font-playball text-2xl mt-12'>{quote.en}</div>
